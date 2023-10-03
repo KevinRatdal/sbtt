00b50ef101a22e32d6de40ce5bbc4f4ea0cd5ccb
Kevin Ratdal <kevin.ratdal@smartm.no>
2023-10-03 10:50:03 +0200

    add api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { fetchData } from './api/common'
 
 function App() {
-
+  const [data, setData] = useState(null)
+  // const iframeRef = useRef(null)
+  useEffect(() => {
+    if (!data) {
+      fetchData().then(d => {
+        setData(d)
+      })
+    }
+  }, [data])
+  const vikingData = data?.teams?.find?.(d => d?.id === 7)
   return (
     <>
-      <h1>
-       Not yet...
+      <h1 style={{userSelect: 'none'}}>
+        {(vikingData?.place === 1) ? 'Yes sir!': 'Not yet...' }
       </h1>
+      { vikingData &&
+        <p style={{ userSelect: 'none' }}>
+          {vikingData?.name} Placement: {vikingData?.place}
+        </p>
+      }
     </>
   )
 }
---
+++ src/api/common.js
@@ -0,0 +1,4 @@
+export const fetchData = async () => {
+  const res = await fetch('https://v2api.nifs.no/stages/691759/table/')
+  return await res.json()
+}
