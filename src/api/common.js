f5d625caedb2230590bd14060c41cf2e598433f2
Kevin Ratdal <43454312+KevinRatdal@users.noreply.github.com>
2024-03-25 11:13:09 +0100

    Update common.js
    Update api to use 2024 stats

--- src/api/common.js
+++ src/api/common.js
@@ -1,5 +1,5 @@
 export const fetchData = async () => {
-  // const res = await fetch('https://v2api.nifs.no/stages/691759/table/')
-  // return await res.json()
-  return {}
+  const res = await fetch('https://v2api.nifs.no/stages/694961/table/')
+  return await res.json()
+  // return {}
 }
